27738e1d30bac2bbd112c964031686fa1c8f6c38
James Smith <matthew02@users.noreply.github.com>
2020-06-16 09:22:10 -0500

    Trying three tests at once

--- backend/test_api.py
+++ backend/test_api.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
 import unittest
-import json
 from flask_sqlalchemy import SQLAlchemy
 
 from api.app import create_application
@@ -17,46 +16,61 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_application('Testing')
         self.client = self.app.test_client()
+        #self.ctx = self.app.app_context()
+        #self.ctx.push()
 
     def tearDown(self):
         """Executed after reach test"""
+        db.session.remove()
+        #self.ctx.pop()
+
 
     def test_get_one_question(self):
         """Test getting a specific question."""
         response = self.client.get('/questions/2')
-        print(f'test_get_one_question response is {response}')
+
+        data = response.get_json()
+        #print(f'question data is {data}')
+        #print(f'question mimetype is {response.mimetype}')
         self.assertEqual(response.status_code, 200)
 
-#        data = json.loads(response.data)
-#        self.assertTrue(data['success'])
-#        self.assertEqual(data['totalQuestions'], 19)
-#        self.assertTrue(len(data['categories']))
-#        self.assertTrue(len(data['questions']) == 1)
+        #self.assertTrue(data['success'])
+        #self.assertEqual(data['totalQuestions'], 19)
+        #self.assertTrue(len(data['categories']))
+        #self.assertTrue(len(data['questions']) == 1)
 
-    def test_get_default_page_of_questions(self):
-        """Test getting the default page of questions."""
-        response = self.client.get('/questions')
-        print(f'test_get_default_page_of_questions response is {response}')
-        self.assertEqual(response.status_code, 200)
+
+#    def test_get_default_page_of_questions(self):
+#        """Test getting the default page of questions."""
+#        response = self.client.get('/questions')
+#        print(f'test_get_default_page_of_questions response is {response}')
+#        self.assertEqual(response.status_code, 200)
+##
+##        data = json.loads(response.data)
+##        self.assertTrue(data['success'])
+##        self.assertEqual(data['totalQuestions'], 19)
+##        self.assertTrue(len(data['categories']))
+##        self.assertTrue(len(data['questions']))
 #
-#        data = json.loads(response.data)
-#        self.assertTrue(data['success'])
-#        self.assertEqual(data['totalQuestions'], 19)
-#        self.assertTrue(len(data['categories']))
-#        self.assertTrue(len(data['questions']))
-
-
+#
     def test_get_categories(self):
         """Test getting the list of trivia categories."""
         response = self.client.get('/categories')
-        print(f'test_get_categories response is {response}')
         self.assertEqual(response.status_code, 200)
-#
-#        data = json.loads(response.data)
-#        self.assertTrue(data['success'])
-#        self.assertEqual(data['totalQuestions'], 19)
-#        self.assertTrue(len(data['categories']))
-#        self.assertTrue(len(data['questions']))
+
+        data = response.get_json()
+        #print(f'categories data is {data}')
+        #print(f'categories mimetype is {response.mimetype}')
+
+
+    def test_get_category_of_questions(self):
+        """Test getting a list of trivia questions by category."""
+        response = self.client.get('/categories/2/questions')
+
+        data = response.get_json()
+        #print(f'category questions data is {data}')
+        #print(f'category questions mimetype is {response.mimetype}')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
